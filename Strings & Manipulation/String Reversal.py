e95cb3e454455eeec455a4b57fb365cb785a0292
raj <rajpatanvadiya5060@gmail.com>
2026-08-20 13:30:02 +0530

    add

--- Strings & Manipulation/String Reversal.py
+++ Strings & Manipulation/String Reversal.py
@@ -1,11 +1,17 @@
 #Reverse a string manually without using built-in slicing ([::-1]) or reversed().
-def reverse_string(text):
-    reversed_text = ""
-    for char in text:
-        # Prepend the character to the existing string
-        reversed_text = char + reversed_text  
-    return reversed_text
+# def reverse_string(text):
+#     reversed_text = ""
+#     for char in text:
+#         # Prepend the character to the existing string
+#         reversed_text = char + reversed_text  
+#     return reversed_text
 
-# Example usage:
-print(reverse_string("hello"))  # Output: "olleh"
+# # Example usage:
+# print(reverse_string("hello"))  # Output: "olleh"
+def reversestring(s):
+    reverse_string = "" 
+    for chr in s:
+        reverse_string = chr + reverse_string
 
+    return reverse_string
+print(reversestring("raj thakor"))
